77a305ec95a45ddaa87fa2024c06808c3bb6ea37
Giovanna Vilaza <giovilaza@gmail.com>
2016-03-10 22:55:23 +0100

    first version
    data about sweden in 2012 and 2005

---
+++ treemap/flare.js
@@ -0,0 +1,37 @@
+// stupid local file access lockdown...I just want my data!
+flare_json = {
+ "name": "flare",
+ "children": [
+
+
+  {
+   "name": "analytics",
+   "children": [
+    {"name": "cluster", "size": 3938},
+    {"name": "graph", "size": 5731},
+    {"name": "optimization", "size": 7074}
+   ]
+  },
+
+  {
+   "name": "animate",
+   "children": [
+    {"name": "Easing", "size": 17010},
+    {"name": "FunctionSequence", "size": 5842},
+    {"name": "Tween", "size": 6006}
+   ]
+  },
+
+  {
+   "name": "data",
+   "children": [
+    {"name": "DataField", 
+    "children": [
+      {"name": "DataSchema", "size": 2165},
+      {"name": "DataSet", "size": 586}
+      ]
+    }
+   ]
+  }
+ ]
+}
